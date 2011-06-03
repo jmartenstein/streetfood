ea608b15ff87fc78a85457c8e7fee3a6f3916294
Justin Martenstein <jmartenstein@gmail.com>
2011-06-02 21:53:13 -0700

    Fix formatting

--- model/neighborhood.rb
+++ model/neighborhood.rb
@@ -14,34 +14,34 @@ ActiveRecord::Base.logger = Logger.new(STDERR)
 # create the connection to our database
 dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection(
-	dbconfig['production']
+   dbconfig['production']
 )
 
 class Neighborhood < ActiveRecord::Base
-	has_many :locations
-	has_many :stops, :through => :locations
+   has_many :locations
+   has_many :stops, :through => :locations
 
 def self.up
 
-	ActiveRecord::Schema.define do
-		create_table :neighborhoods do | table |
-			table.string 	:name
-		end
-	end
+   ActiveRecord::Schema.define do
+      create_table :neighborhoods do | table |
+         table.string 	:name
+      end
+   end
 
 end
 
 def self.down
-	ActiveRecord::Schema.drop_table :neighborhoods
+   ActiveRecord::Schema.drop_table :neighborhoods
 end
 
 def self.import
-	
-	CSV.foreach("./data/neighborhoods.csv") do | line |
-		record = self.create(
-			:name 	=> line[0]
-		)
-	end  # CSV.foreach
+
+   CSV.foreach("./data/neighborhoods.csv") do | line |
+      record = self.create(
+         :name 	=> line[0]
+      )
+   end  # CSV.foreach
 
 end
 
