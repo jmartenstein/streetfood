69ea12743d5e8c6dee5d8ca19728c4d30d9d8f8d
Justin Martenstein <jmartenstein@gmail.com>
2011-06-02 21:30:21 -0700

    Fixed formatting

--- model/stop.rb
+++ model/stop.rb
@@ -18,23 +18,23 @@ ActiveRecord::Base.logger = Logger.new(STDERR)
 # create the connection to our database
 dbconfig = YAML.load(File.read('config/database.yml'))
 ActiveRecord::Base.establish_connection(
-	dbconfig['production']
+   dbconfig['production']
 )
 
 class Stop < ActiveRecord::Base
-	belongs_to 	:truck
-	belongs_to	:location
+   belongs_to 	:truck
+   belongs_to	:location
 
 def self.up
 
    ActiveRecord::Schema.define do
       create_table :stops do | table |
-         table.references	:truck
-         table.references 	:location
-         table.string 		:date    
-         table.string 		:hours     
-         table.boolean 		:suppressed  
-		end
+         table.references  :truck
+         table.references  :location
+         table.string      :date    
+         table.string      :hours     
+         table.boolean     :suppressed  
+      end
    end
 
 end
@@ -57,17 +57,17 @@ def self.import
       #   date = Date.strptime(line[3], "%m/%d/%Y")
       #end
 
-		# make it first, because we're assuming that 'name' is unique;
-		# should put some sort of architecture in to back that up
-		truck = Truck.where('name LIKE ?', line[0]).first
-		location = Location.where('name LIKE ?', line[1]).first
+      # make it first, because we're assuming that 'name' is unique;
+      # should put some sort of architecture in to back that up
+      truck = Truck.where('name LIKE ?', line[0]).first
+      location = Location.where('name LIKE ?', line[1]).first
 
       # now create records for every line in the csv
       record = self.create(
-         :truck_id		=> truck.id,
+         :truck_id      => truck.id,
          :location_id   => location.id,
-         :date        	=> line[2],
-         :hours       	=> line[3]
+         :date          => line[2],
+         :hours         => line[3]
       )
 
    end
@@ -85,17 +85,17 @@ end
 
 def self.by_neighborhood( hood )
 
-	# grab the list of locations for a particular neighborhood
-	neighborhood = Neighborhood.find_by_name(hood)
-	locations = Location.where( 'neighborhood_id LIKE ?', neighborhood.id )
-	stops = []
+   # grab the list of locations for a particular neighborhood
+   neighborhood = Neighborhood.find_by_name(hood)
+   locations = Location.where( 'neighborhood_id LIKE ?', neighborhood.id )
+   stops = []
 
-	# for each location, list out the stops
-	locations.each do | location |
-		stops.push(self.where( 'location_id LIKE ?', location.id))
-	end
+   # for each location, list out the stops
+   locations.each do | location |
+      stops.push(self.where( 'location_id LIKE ?', location.id))
+   end
 
-	return stops
+   return stops
 
 end
 
