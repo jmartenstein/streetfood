5b33b6c7f2491ff6b35fce94703f850127676952
Justin Martenstein <jmartenstein@gmail.com>
2011-07-02 21:55:09 -0700

    Add unit test files

---
+++ test/test_food.rb
@@ -0,0 +1,32 @@
+# test_food.rb
+
+require './model/food'
+require 'test/unit'
+
+class FoodTest < Test::Unit::TestCase
+
+def setup
+end # def setup
+
+def teardown
+end # def teardown
+
+def test_up1
+
+   assert_equal(1,0)
+
+end # def test_up1
+
+def test_down1
+
+   assert_equal(1,0)
+
+end  # def test_down1
+
+def test_import1
+
+   assert_equal(1,0)
+
+end
+
+end   # class StopTest
---
+++ test/test_stop.rb
@@ -0,0 +1,85 @@
+# test_stop.rb
+
+require './model/stop'
+require 'test/unit'
+
+class StopTest < Test::Unit::TestCase
+
+def setup
+
+   #Location.up
+   #Truck.up
+   #Stop.up
+   #Neighborhood.up
+
+   @location_name1       = "location foo"
+   @truck_name1          = "truck foo"
+   @neighborhood_name1   = "neighborhood foo"
+
+   # create some data in the database; this isn't technicall a "pure" unit
+   # test, but it'll do for now
+   #neighborhood1 = Neighborhood.new( :name => neighborhood_name1 ).save
+   #location1 = Location.new( :name => location_name1, :neighborhood_id => 1 ).save
+   #truck1 = Truck.new( :name => truck_name1 ).save
+   #stop1 = Stop.new( :truck_id => 1, :location_id => 1, :date => Date.today ).save
+
+
+end # def setup
+
+def teardown
+
+   #Stop.down
+   #Truck.down
+   #Location.down
+   #Neighborhood.down
+
+end # def teardown
+
+def test_location1
+
+   stop1 = Stop.find(1)
+   assert_equal( @location_name1, stop1.location.name ) 
+
+end # def test_location1
+
+def test_truck1
+
+   stop1 = Stop.find(1)
+   assert_equal( @truck_name1, stop1.truck.name )
+
+end # def test_truck1
+
+def test_neighborhood1
+
+   stop1 = Stop.find(1)
+   assert_equal( @neighborhood_name1, stop1.location.neighborhood.name )
+
+end # def test_neighborhood1
+
+def test_by_neighborhood1
+
+   temp_stop_list = Stop.by_neighborhood( @neighborhood_name1 )
+
+   stop1 = Stop.find(1)
+   neighborhood_stop_list = [ stop1 ]
+
+   i = 0
+   temp_stop_list.each do |stop|
+      assert_equal(stop.id, neighborhood_stop_list[i].id)
+      i = i + 1
+   end
+   
+end
+
+def test_by_date1
+
+   stop1 = Stop.find(1)
+   expected_stop_list = [ stop1 ]
+
+   actual_stop_list = Stop.by_date( Date.today )
+
+   assert_equal(expected_stop_list, actual_stop_list)
+
+end
+
+end   # class StopTest
